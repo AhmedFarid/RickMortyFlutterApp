51f22cc58f811fce145d56b75cfceca7300ec7ca
Ahmed Farid <ahmed.farid@con.purecs.com>
2025-09-14 15:20:38 +0300

    add MockRemote file for test

---
+++ test/mocks.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:rick_morty_app/src/data/datasources/character_remote_ds.dart';
+
+class MockRemote extends Mock implements CharacterRemoteDataSource {}
